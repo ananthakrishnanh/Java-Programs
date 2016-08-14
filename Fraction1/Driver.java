0a24a90d6e03ce7b50de650e0bf8b59bee164711
ananthakrishnanh <ananthakrishnanh97@gmail.com>
2016-08-14 15:46:01 +0530

    Driver Class

---
+++ Fraction1/Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+	public static void main(String[] args){
+	Fraction f=new Fraction();
+	f.setNumerator(2);
+	f.setDenominator(3);
+	System.out.println("The fraction is " + f.getNumerator() + "/"   + f.getDenominator());
+	}
+}
